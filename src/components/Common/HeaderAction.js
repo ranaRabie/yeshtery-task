d7ee0b116e19203309a349ed602cfc5c230ef5bb
RanaRabie-Jumia <rana.rabie@jumia.com>
2022-12-14 16:02:46 +0200

    fix cart click

--- src/components/Common/HeaderAction.js
+++ src/components/Common/HeaderAction.js
@@ -1,10 +1,10 @@
 function HeaderAction({ onClick, ico, name, children }) {
     return ( 
-        <a href="/" className="main-header__actions-item" onClick={onClick}>
+        <div className="main-header__actions-item" onClick={onClick}>
             <div className="main-header__actions-item-ico">{ ico }</div>
             <div className="main-header__actions-item-name">{ name }</div>
             { children }
-        </a>
+        </div>
      );
 }
 
