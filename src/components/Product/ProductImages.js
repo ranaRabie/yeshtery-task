b5a1a27f575ef239e4776b9b823a90492fb2d97c
RanaRabie-Jumia <rana.rabie@jumia.com>
2022-12-14 15:55:36 +0200

    fix alt attr in image tags

--- src/components/Product/ProductImages.js
+++ src/components/Product/ProductImages.js
@@ -2,10 +2,10 @@ function ProductImages({ images }) {
     return (  
         <div className="single-product__images">
             <div className="single-product__images-single-img">
-                <img src={images[0]} alt="product main image" />
+                <img src={images[0]} alt="product main" />
             </div>
             <div className="single-product__images-single-thumbs">
-                {images.map((img, key) => <div key={key}><img src={img} alt="product thumb image" /></div>)}
+                {images.map((img, key) => <div key={key}><img src={img} alt="product thumb" /></div>)}
             </div>
         </div>
     );
