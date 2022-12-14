07b50da5bddb66d19cb0ae6e96a7d77cb5c3aa68
RanaRabie-Jumia <rana.rabie@jumia.com>
2022-12-14 15:53:58 +0200

    fix alt attr in image tags

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,7 @@
 function CartItem({ item }) {
     return ( 
         <div className="cart__dropdown-list-item">
-            <div className="cart__dropdown-list-item-image"><img src={ item.image } /></div>
+            <div className="cart__dropdown-list-item-image"><img src={ item.image } alt="product image" /></div>
             <div className="cart__dropdown-list-item-body">
                 <div className="cart__dropdown-list-item-name">{ item.name }</div>
                 <div className="cart__dropdown-list-item-dets">size:{ item.size }, count: { item.count }</div>
--- src/components/Product/ProductImages.js
+++ src/components/Product/ProductImages.js
@@ -2,10 +2,10 @@ function ProductImages({ images }) {
     return (  
         <div className="single-product__images">
             <div className="single-product__images-single-img">
-                <img src={images[0]} />
+                <img src={images[0]} alt="product main image" />
             </div>
             <div className="single-product__images-single-thumbs">
-                {images.map((img, key) => <div key={key}><img src={img} /></div>)}
+                {images.map((img, key) => <div key={key}><img src={img} alt="product thumb image" /></div>)}
             </div>
         </div>
     );
